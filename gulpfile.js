9f2c260794372c3c5d21ab485c78ea2a0a3c2a02
rdrew <rdrew@upei.ca>
2021-12-10 16:19:05 -0400

    1st

---
+++ gulpfile.js
@@ -0,0 +1,79 @@
+//@format
+const yaml = require('js-yaml');
+const fs = require('fs');
+const { SITE, PORT, BSREWRITE, PATHS } = loadConfig();
+//var server = require('browser-sync').create();
+//global.server = server;
+const gulp = require('gulp');
+const plumber = require('gulp-plumber');
+const autoprefixer = require('gulp-autoprefixer');
+const sass = require('gulp-sass')(require('sass'));
+const browsersync = require('browser-sync').create();
+const sourcemaps = require('gulp-sourcemaps');
+
+function loadConfig() {
+  var ymlFile = fs.readFileSync('config.yml', 'utf8');
+  return yaml.load(ymlFile);
+}
+
+// BrowserSync
+function bsInit(done) {
+  browsersync.init({
+    proxy: SITE.Remote.Url,
+    serveStatic: ['.'],
+    files: PATHS.Watch,
+    plugins: ['bs-rewrite-rules'],
+    rewriteRules: [
+      {
+        match: BSREWRITE.Css.Match,
+        replace: BSREWRITE.Css.Replace
+      },
+      {
+        match: BSREWRITE.Js.Match,
+        replace: BSREWRITE.Js.Replace
+      }
+    ]
+  });
+  done();
+}
+
+// BrowserSync Reload
+function bsReload(done) {
+  browsersync.reload();
+  done();
+}
+
+// Compile CSS
+function styles() {
+  'use strict';
+  return gulp
+    .src(PATHS.Scss.Dir + '/**/*.scss')
+    .pipe(plumber())
+    .pipe(sourcemaps.init())
+    .pipe(
+      sass
+        .sync({
+          includePaths: PATHS.Scss.Libraries
+        })
+        .on('error', sass.logError)
+    )
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest(PATHS.Css.Dir))
+    .pipe(browsersync.stream());
+}
+
+// Watch Files
+function watchFiles() {
+  'use strict';
+  gulp.watch(PATHS.Scss.Dir + '/**/*.scss', styles);
+}
+
+// Group complex tasks
+const build = gulp.parallel(styles);
+const watch = gulp.series(styles, gulp.parallel(watchFiles, bsInit));
+
+// Export tasks
+exports.build = build;
+exports.styles = styles;
+exports.watch = watch;
+exports.default = watch;
